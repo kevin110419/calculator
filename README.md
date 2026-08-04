d3157ee5c088f4d35d6bb1733f9f97333720211d
kevin110419 <kevin9448@gmail.com>
2026-08-04 16:18:05 +0900

    dd

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # calculator
 Git 실습용
+
+# 계산기 프로젝트
+### 1. 기능소개
+- 더하기
+```
+def sum(a, b):
+    return a + b
+```
+- 빼기
+```
+def sub(a, b):
+    return a - b
+```
+- 곱하기
+```
+def mul(a, b):
+    return a *+* b
+```
+- 나누기
+```
+def divi(a, b):
+    return a/b
+```
+###2. 개발자
+- 감사
+----
+감사
+<<<<<<< HEAD
+dd
+=======
+
+dd
+>>>>>>> 
--- calc.py
+++ calc.py
@@ -1,2 +1,26 @@
-def sum(a,b):
-    return a+b
+# +
+def sum(a, b):
+    return a + b
+
+# -
+def sub(a, b):
+    return a - b
+
+# *
+def mul(a, b):
+    return a * b
+
+# **
+def square(a, b):
+    return a ** b
+
+# %
+def mod(a, b):
+    return a % b
+
+# /
+def div(a, b):
+    if b == 0:
+        return 0
+    else:
+        return a / b
